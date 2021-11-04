3aee9074bef07619c0b3234a55d8033b5460415c
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-04 19:23:22 +0900

    implement GET method

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+'use strict';
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hey there.');
+
+// demo database TO BE deleted
+const genres = [
+  { id: 1, name: 'Action' },  
+  { id: 2, name: 'Horror' },  
+  { id: 3, name: 'Romance' },  
+];
+
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
 });
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) res.status(404).send('The genre is not found.');
+  res.send(
+    `The genre is found!
+    genre ID: ${genre.id}
+    genre name: ${genre.name}`
+    );
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
