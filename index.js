53577d26e82893d97e7440a175ac7d897b1989fa
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-04 19:32:23 +0900

    create PUT method

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  const { error } = validateGenre(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  genre.name = req.body.name; 
+  res.send(genre);
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
