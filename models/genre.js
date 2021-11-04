c15a2921b9c476c3b2611df83c77f69ee0bdd4e3
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-04 21:27:14 +0900

    modularize model and validation

---
+++ models/genre.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+module.exports.Genre = Genre;
+module.exports.validate = validateGenre;
