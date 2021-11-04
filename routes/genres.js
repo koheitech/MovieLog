561d5d7922d760047d29f49c38fdbe3d78f977fe
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-04 19:57:46 +0900

    modularize routers

---
+++ routes/genres.js
@@ -0,0 +1,68 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+// demo database TO BE deleted
+const genres = [
+  { id: 1, name: 'Action' },  
+  { id: 2, name: 'Horror' },  
+  { id: 3, name: 'Romance' },  
+];
+
+router.get('/', (req, res) => {
+  res.send(genres);
+});
+
+router.get('/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) res.status(404).send('The genre is not found.');
+  res.send(
+    `The genre is found!
+    genre ID: ${genre.id}
+    genre name: ${genre.name}`
+    );
+});
+
+router.post('/', (req, res) => {
+  const { error } = validateGenre(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+router.put('/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  const { error } = validateGenre(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  genre.name = req.body.name; 
+  res.send(genre);
+});
+
+router.delete('/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+module.exports = router;
