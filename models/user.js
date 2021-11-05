c85ba9b306362bf5d081243fc5af30ec93257ccb
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-05 11:20:59 +0900

    create router for authenticating user

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ function validateUser(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(255).required()
   };
 
   return Joi.validate(user, schema);
