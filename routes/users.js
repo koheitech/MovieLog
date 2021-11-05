2393f6f90df2a6bb7ae2edc86087b253cb3d56a8
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-05 11:01:17 +0900

    hash password with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
@@ -15,6 +16,9 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10); //create salt with round 10
+  user.password = await bcrypt.hash(user.password, salt);
+
   await user.save();
   
   res.send(_.pick(user, ['_id', 'name', 'email']));
