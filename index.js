c6feca6f46e823d30b762e206c78272f9e67c8fe
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 10:48:35 +0900

    extract routes module (separation of concerns)

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const config = require('config');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const error = require('./middleware/error');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
 
 process.on('uncaughtException', (ex) => {
   winston.error(ex.message, ex);
@@ -37,15 +34,6 @@ if (!config.get('jwtPrivateKey')) {
 mongoose.connect('mongodb://localhost/movielog', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.err('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
