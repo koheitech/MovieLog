d3b6e2976a80883881d8a4a45ad192c6bd4dc655
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 11:08:33 +0900

    extract logging module (SoC)

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
+
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -9,19 +7,10 @@ Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
 
-winston.handleExceptions(
-  new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-
-process.on('unhandledRejection', (ex) => {
-  throw ex;
-});
-
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/movielog' });
 
 
 if (!config.get('jwtPrivateKey')) {
