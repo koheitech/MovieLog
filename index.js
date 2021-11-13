fb2386a757c231b431c7c0fcc3c1ae6947aeb02a
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 11:14:33 +0900

    replace 'console.log()' with 'winston.info' for better logging performance

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const winston = require('winston');
 
 require('./startup/logging');
 require('./startup/routes')(app);
@@ -10,4 +11,4 @@ require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
