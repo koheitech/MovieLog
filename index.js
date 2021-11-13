887a6468bafd4f88642089ae01c7a8603955f814
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 10:35:23 +0900

    modify those two handlers so that they exit the program

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const express = require('express');
 const app = express();
 
 process.on('uncaughtException', (ex) => {
-  console.log('WE GOT AN UNCAUGHT EXCEPTION');
   winston.error(ex.message, ex);
+  process.exit(1);
 });
 
 process.on('unhandledRejection', (ex) => {
-  console.log('WE GOT AN UNHANDLED REJECTION');
   winston.error(ex.message, ex);
+  process.exit(1);
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
