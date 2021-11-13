d33a8621422ce09c2b60ca084f4d373f07640840
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 11:02:44 +0900

    refactor unaught exception and unhandled rejection handler with winston

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-process.on('uncaughtException', (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
+winston.handleExceptions(
+  new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
 process.on('unhandledRejection', (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
+  throw ex;
 });
 
 
