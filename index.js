4771beba5c2da641b2277e9c36e642df60711b92
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-13 11:13:10 +0900

    extract validation module (SoC)

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-
 const express = require('express');
 const app = express();
 
@@ -10,6 +7,7 @@ require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
