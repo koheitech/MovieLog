9b9f381f90bcadd151c49d6ff468f53a6ce3745c
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-14 08:12:24 +0900

    modfy the integration test so that it can populate test data before the test and clean it up after the test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
+
 let server;
 
 describe('/api/genres', () => {
@@ -10,12 +12,23 @@ describe('/api/genres', () => {
    * which will crash the next test or next server launch.
    */
   beforeEach(() => { server = require('../../index'); });
-  afterEach(() => { server.close(); });
+  afterEach(async () => { 
+    server.close(); 
+    await Genre.remove({});
+  });
 
   describe('GET /', () => {
     it('should return all the genres', async () => {
+      await Genre.collection.insertMany([
+        { name: 'genre1'},
+        { name: 'genre2'}
+      ]);
       const res = await request(server).get('/api/genres');
+      
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
     });
   });
   // describe('GET /', () => {});
